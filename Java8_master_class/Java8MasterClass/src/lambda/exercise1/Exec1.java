2e3967e9674995111b9fc516b139418f54a09301
2194063 <jeromegarcia.16@gmail.com>
2023-05-29 18:58:28 +0800

    exercise 1

---
+++ Java8_master_class/Java8MasterClass/src/lambda/exercise1/Exec1.java
@@ -0,0 +1,25 @@
+package lambda.exercise1;
+public class Exec1 {
+        interface Counter {
+            abstract Integer count(String str);
+        }
+        public static void main(String[]args){
+
+            Counter lengthCounter = str -> {
+                int length = str.length();
+                return length;
+            };
+            Counter letterCounter = str -> {
+                int letters = 0;
+                char[] ch = str.toCharArray();
+                for (int j = 0; j < str.length(); j++){
+                    if (Character.isLetter(ch[j])){
+                        letters++;
+                    }
+                }
+                return letters;
+            };
+            System.out.println(lengthCounter.count("There are 8 planets in our solar system!"));
+            System.out.println(letterCounter.count("There are 8 planets in our solar system!"));
+        }
+    }
