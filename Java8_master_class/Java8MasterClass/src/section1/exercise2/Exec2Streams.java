939ae8103f4de629b9fe08463918902eb3e19d1f
2194063 <jeromegarcia.16@gmail.com>
2023-05-31 18:28:15 +0800

    exercise 2

---
+++ Java8_master_class/Java8MasterClass/src/section1/exercise2/Exec2Streams.java
@@ -0,0 +1,38 @@
+package section1.exercise2;
+
+import java.util.Arrays;
+
+public class Exec2Streams {
+
+    public static void main(String[] args) {
+//        Integer[] numArr = { 7, 5, 901, 175, 8, 100, 21, 16, 3, 26, 34, 89, 82, 1, 1000, 101 };
+        Integer[] numArr = { 7, 5, 8, 901, 175, 100, 21, 16, 3, 26, 34, 89, 1, 1000, 101 };
+        solution(numArr);
+    }
+    public static void solution(Integer[] numArr) {
+        double[] filteredNum = Arrays.stream(numArr)
+                .filter(num -> num <= 100)
+                .sorted((a, b) -> b - a)
+                .mapToDouble(num -> (double) num)
+                .toArray();
+        System.out.println("Filtered and sorted numbers : ");
+        for (double num : filteredNum) {
+            System.out.print(num + "  ");
+
+        }
+        if (filteredNum.length %2 == 0 ){
+            int middleIndex = filteredNum.length / 2-1;
+            double firstMiddleNumber = filteredNum[middleIndex];
+            System.out.println("\nFirst Middle Number " + firstMiddleNumber);
+        } else {
+            int middleIndex = filteredNum.length / 2;
+            double firstMiddleNumber = filteredNum[middleIndex];
+            System.out.println("\nFirst Middle Number " + firstMiddleNumber);
+        }
+    }
+
+}
+
+
+
+
