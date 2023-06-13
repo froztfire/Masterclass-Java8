f57ccf70d6b17bbc3ec0ec2d9e01ef78071808cb
2194063 <jeromegarcia.16@gmail.com>
2023-06-13 22:04:29 +0800

    fixed the christmas time PH to US conversion

--- Java8_master_class/Java8MasterClass/src/java8/exercise4/DateTimeManipulation.java
+++ Java8_master_class/Java8MasterClass/src/java8/exercise4/DateTimeManipulation.java
@@ -10,23 +10,24 @@ public class DateTimeManipulation {
         ZoneId usTimeZone = ZoneId.of("America/Chicago");
 
         ZonedDateTime phDateTime = ZonedDateTime.now(phTimeZone);
-        ZonedDateTime usDateTime = phDateTime.withZoneSameInstant(usTimeZone);
-
         LocalDateTime currentDateTime = phDateTime.toLocalDateTime();
-        LocalDateTime christmasDateTime = LocalDateTime.of(currentDateTime.getYear(), Month.DECEMBER, 25, 0, 0);
+        LocalDateTime christmasDateTimePH = LocalDateTime.of(currentDateTime.getYear(),
+                Month.DECEMBER, 25, 0, 0);
+        ZonedDateTime christmasDateTimeEST = christmasDateTimePH.atZone(phTimeZone).withZoneSameInstant(usTimeZone);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy - hh:mm a");
-        String phXmasTime = christmasDateTime.format(formatter);
+        String phXmasTime = christmasDateTimePH.format(formatter);
+        String usXmasTime = christmasDateTimeEST.format(formatter);
 
         System.out.println("PH: " + phXmasTime + "  ("+phTimeZone+")");
-        System.out.println("US: " + usDateTime.plusMonths(6).plusDays(17).format(formatter) + "  ("+usTimeZone+")");
+        System.out.println("US: " + usXmasTime + "  ("+usTimeZone+")");
 
-        if (currentDateTime.isAfter(christmasDateTime)) {
-            christmasDateTime = christmasDateTime.plusYears(1);
+        if (currentDateTime.isAfter(christmasDateTimePH)) {
+            christmasDateTimePH = christmasDateTimePH.plusYears(1);
         }
-        long months = ChronoUnit.MONTHS.between(currentDateTime, christmasDateTime);
-        long days = ChronoUnit.DAYS.between(currentDateTime.plusMonths(months), christmasDateTime.plusDays(1));
-        long remainingDays = currentDateTime.plusDays(-1).until(christmasDateTime, ChronoUnit.DAYS);
+        long months = ChronoUnit.MONTHS.between(currentDateTime, christmasDateTimePH);
+        long days = ChronoUnit.DAYS.between(currentDateTime.plusMonths(months), christmasDateTimePH.plusDays(1));
+        long remainingDays = currentDateTime.plusDays(-1).until(christmasDateTimePH, ChronoUnit.DAYS);
         System.out.println("Remaining " + months + " month/s and " + days + " days before Christmas!"  );
         System.out.println("Days before Christmas: " + remainingDays);
     }
